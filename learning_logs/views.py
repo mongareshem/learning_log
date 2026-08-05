bdfbda500e3c6bb4dd6642f33743a404d0d0c8a2
Shem <mongareshem1@gmail.com>
2026-08-06 01:40:49 +0300

    Write the topics url and views

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 
+from .models import Topic
+
 def index(request):
     """The home page for learning_log"""
 
     return render(request, 'learning_logs/index.html')
+
+
+def topics(request):
+    """Show all topics."""
+    topics_available = Topic.objects.order_by('date_added')
+    context = {'topics': topics_available}
+
+    return render(request, 'learning_logs/topics.html', context)
